61b128edf355dc607290eb16cc5ba124b11fbcab
Olivier Huguenot <olivier.huguenot@students.fhnw.ch>
2024-03-18 23:07:18 +0100

    Calculation of D

--- src/main/java/ch/mada/Main.java
+++ src/main/java/ch/mada/Main.java
@@ -1,6 +1,11 @@
 package ch.mada;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -9,8 +14,8 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         // Generate Random Prime Number
-        BigInteger p = BigInteger.probablePrime(2048, random);
-        BigInteger q = BigInteger.probablePrime(2048, random);
+        BigInteger p = BigInteger.valueOf(11);
+        BigInteger q = BigInteger.valueOf(3);
 
         // p und q multiplizieren und n erhalten
         BigInteger n = p.multiply(q);
@@ -24,6 +29,17 @@ public class Main {
         System.out.println("E: " + e);
 
         //d berechnen Mithilfe des euklidischen Algorithmus
+        BigInteger d = calculateD(nPHI, e);
+        System.out.println("D: " + d);
+
+        //Inhalt verschlüssel
+        BigInteger encrypted = encrypt(e, "Hello",n);
+        System.out.println("Encrypted: " + encrypted);
+
+        //Inhalt entschlüsseln
+        System.out.println(new String(encrypted.modPow(d, n).toByteArray(), StandardCharsets.UTF_8));
+
+
 
 
 
@@ -38,15 +54,6 @@ public class Main {
         q = q.subtract(BigInteger.ONE);
         BigInteger n = p.multiply(q);
         return n;
-
-
-        /*List<BigInteger> result = new ArrayList<>();
-        BigInteger t = BigInteger.valueOf(0);
-        while(!n.equals(t)) {
-            if(n.gcd(t).equals(BigInteger.valueOf(1))) {
-                result.add(t);
-            }
-            t = t.add(BigInteger.ONE);*/
     }
 
     public static BigInteger calculateE (BigInteger nPHI) {
@@ -60,16 +67,54 @@ public class Main {
             }
             r = r.add(BigInteger.ONE);
         }
-
         return BigInteger.valueOf(-1);
+    }
 
-        /*while (!nPHI.equals(t)) {
-            if (nPHI.gcd(t).equals(BigInteger.valueOf(1))) {
-                result.add(t);
-            }
-            t = t.add(BigInteger.ONE);
+
+    //Erweiterter Euklidischer Algorithmus
+    public static BigInteger calculateD(BigInteger nPHI, BigInteger e) {
+        BigInteger a = nPHI;
+        BigInteger b = e;
+        BigInteger x0 = BigInteger.ONE;
+        BigInteger x1 = BigInteger.ZERO;
+        BigInteger y0 = BigInteger.ZERO;
+        BigInteger y1 = BigInteger.ONE;
+        BigInteger q;
+        BigInteger r;
+
+        while(!b.equals(BigInteger.ZERO)) {
+            q = a.divide(b);
+            r = a.mod(b);
+            a = b;
+            b = r;
+            BigInteger temporaryX = x0.subtract(q.multiply(x1));
+            BigInteger temporaryY = y0.subtract(q.multiply(y1));
+            x0 = x1;
+            y0 = y1;
+            x1 = temporaryX;
+            y1 = temporaryY;
         }
-        return result; */
+        return y0;
+    }
+
+    public static BigInteger encrypt(BigInteger e, String x, BigInteger n) {
+        //Initialisierung
+        byte[] bytesE = e.toByteArray();
+        int i = bytesE.length - 1;
+        byte[] byteK = x.getBytes(StandardCharsets.UTF_8);
+        BigInteger h = BigInteger.valueOf(1);
+
+        //Iteriertes Quadrieren
+        BigInteger k = new BigInteger(byteK);
+        while (i >= 0) {
+            if(bytesE[i] == 1) {
+                h = h.multiply(k).mod(n);
+            }
+            k = k.pow(2).mod(n);
+            i = i - 1;
+            i--;
+        }
+        return h;
     }
 
     public static BigInteger generateRandomBigIntBetween(BigInteger min, BigInteger max) {
@@ -81,7 +126,6 @@ public class Main {
             result = new BigInteger(numBits, random);
         } while (result.compareTo(min) < 0 || result.compareTo(max) > 0);
 
-        System.out.println("Random: " + result);
         return result;
     }
 }
