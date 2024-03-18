f960c5e83cc460cd0bda75a216e3ecb802704904
Olivier Huguenot <olivier.huguenot@students.fhnw.ch>
2024-03-18 18:57:58 +0100

    Generate Random Prim Numbers

--- src/main/java/ch/mada/Main.java
+++ src/main/java/ch/mada/Main.java
@@ -1,7 +1,13 @@
 package ch.mada;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Random random = new Random();
+        BigInteger p = BigInteger.probablePrime(2048, random);
+        BigInteger q = BigInteger.probablePrime(2048, random);
     }
 }
