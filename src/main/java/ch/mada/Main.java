1dd496b27d2184e48e27957cb050a740f6256689
Olivier Huguenot <olivier.huguenot@students.fhnw.ch>
2024-03-18 21:08:14 +0100

    Calculation of E corrected

--- src/main/java/ch/mada/Main.java
+++ src/main/java/ch/mada/Main.java
@@ -7,17 +7,26 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Random random = new Random();
         // Generate Random Prime Number
         BigInteger p = BigInteger.probablePrime(2048, random);
         BigInteger q = BigInteger.probablePrime(2048, random);
 
-        // Multiply p and q to receive n
+        // p und q multiplizieren und n erhalten
         BigInteger n = p.multiply(q);
 
         //PHI von n berechnen
         BigInteger nPHI = phiOfn(p,q);
+        System.out.println("PHI von n: " + nPHI);
+
+        //Menge an GGTs von phiOfn
+        BigInteger e = calculateE(nPHI);
+        System.out.println("E: " + e);
+
+        //d berechnen Mithilfe des euklidischen Algorithmus
+
+
+
 
 
     }
@@ -37,10 +46,42 @@ public class Main {
             if(n.gcd(t).equals(BigInteger.valueOf(1))) {
                 result.add(t);
             }
-            t.add(BigInteger.ONE);*/
+            t = t.add(BigInteger.ONE);*/
     }
 
-    public static List<BigInteger> calculateE (BigInteger nPHI) {
-        return null;
+    public static BigInteger calculateE (BigInteger nPHI) {
+        List<BigInteger> result = new ArrayList<>();
+        BigInteger r = generateRandomBigIntBetween(BigInteger.valueOf(0), nPHI.subtract(BigInteger.ONE));
+
+
+        while (!nPHI.equals(r)) {
+            if (nPHI.gcd(r).equals(BigInteger.valueOf(1))) {
+                return r;
+            }
+            r = r.add(BigInteger.ONE);
+        }
+
+        return BigInteger.valueOf(-1);
+
+        /*while (!nPHI.equals(t)) {
+            if (nPHI.gcd(t).equals(BigInteger.valueOf(1))) {
+                result.add(t);
+            }
+            t = t.add(BigInteger.ONE);
+        }
+        return result; */
+    }
+
+    public static BigInteger generateRandomBigIntBetween(BigInteger min, BigInteger max) {
+        Random random = new Random();
+        int numBits = max.bitLength();
+
+        BigInteger result;
+        do {
+            result = new BigInteger(numBits, random);
+        } while (result.compareTo(min) < 0 || result.compareTo(max) > 0);
+
+        System.out.println("Random: " + result);
+        return result;
     }
 }
